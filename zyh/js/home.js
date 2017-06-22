d6a546a6fc61cb55c65b9bbfff2b450920dfe5b2
STOCompany <598596582@qq.com>
2017-06-22 15:09:37 +0800

    新增JSON

--- zyh/js/home.js
+++ zyh/js/home.js
@@ -1,3 +1,49 @@
 function toShow() {
     self.location = 'show.html';
 };
+
+function callJsonBack() {
+    console.log("'{'hellen':'type'}'");
+    self.location = 'json.html';
+}
+
+function loadXMLDoc() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    console.log(xmlhttp);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("result").innerHTML = syntaxHighlight(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", "json.json", true);
+    xmlhttp.send();
+}
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        console.log('<span class="' + cls + '">' + match + '</span>');
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
