2a6a653311008ca90dbc0b94277072eaff785ae7
STOCompany <598596582@qq.com>
2017-06-22 14:18:55 +0800

    新增跳转

--- js/mobeilcop.js
+++ js/mobeilcop.js
@@ -13,18 +13,18 @@
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
-    
+
     // 一物理像素在不同屏幕的显示效果不一样。要根据devicePixelRatio来修改meta标签的scale,要注释上面的meta标签
-    (function(){
+    (function () {
         return;
-        var dpr = scale =1;
+        var dpr = scale = 1;
         var isIPhone = win.navigator.appVersion.match(/iphone/gi);
         var devicePixelRatio = win.devicePixelRatio;
         if (isIPhone) {
             // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
                 dpr = 3;
-            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
                 dpr = 2;
             } else {
                 dpr = 1;
@@ -33,13 +33,13 @@
             // 其他设备下，仍旧使用1倍的方案
             dpr = 1;
         }
-           scale = 1 / dpr;
-           
-           //
-           var metaEl = "";
-           metaEl = doc.createElement('meta');
-                metaEl.setAttribute('name', 'viewport');
-                metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+        scale = 1 / dpr;
+
+        //
+        var metaEl = "";
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
         if (docEl.firstElementChild) {
             docEl.firstElementChild.appendChild(metaEl);
         } else {
@@ -49,4 +49,8 @@
         }
     })();
 
-})(document, window);    
+})(document, window);
+
+function toZyh() {
+    self.location = 'zyh/index.html'
+}
